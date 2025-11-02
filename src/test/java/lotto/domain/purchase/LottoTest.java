047574f0e7b39c3e8259c897f23760c7eb8513fd
Junseo Bae <ferrater1013@gmail.com>
2025-11-02 21:24:27 +0900

    test(lotto): 로또 번호 포함 여부 확인 기능 테스트 추가

--- src/test/java/lotto/domain/purchase/LottoTest.java
+++ src/test/java/lotto/domain/purchase/LottoTest.java
@@ -1,10 +1,16 @@
 package lotto.domain.purchase;
 
+import lotto.domain.shared.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -32,5 +38,28 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(PurchaseErrorMessage.DUPLICATED_LOTTO_NUMBER.message());
+    }
+
+    @DisplayName("주어진 번호가 로또에 포함되어 있는지 확인한다.")
+    @ParameterizedTest
+    @MethodSource("provideLottoNumberAndResult")
+    void checkLottoNumberIsContainedInLotto(LottoNumber lottoNumber, boolean expected) {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 3, 10, 30, 40, 45));
+
+        // when
+        boolean result = lotto.contains(lottoNumber);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> provideLottoNumberAndResult() {
+        return Stream.of(
+                Arguments.of(new LottoNumber(1), true),
+                Arguments.of(new LottoNumber(2), false),
+                Arguments.of(new LottoNumber(3), true),
+                Arguments.of(new LottoNumber(7), false)
+        );
     }
 }
