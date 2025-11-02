0ab0c4c5d60e7cfc2f96dbde603eb27c3aa82209
Junseo Bae <ferrater1013@gmail.com>
2025-11-02 20:37:08 +0900

    refactor: Money VO 삭제

--- src/main/java/lotto/domain/shared/SharedErrorMessage.java
+++ src/main/java/lotto/domain/shared/SharedErrorMessage.java
@@ -3,7 +3,6 @@ package lotto.domain.shared;
 import lotto.common.ErrorMessage;
 
 public enum SharedErrorMessage implements ErrorMessage {
-    MONEY_NEGATIVE_VALUE("돈은 음수일 수 없습니다."),
     LOTTO_NUMBER_OUT_OF_RANGE("로또 번호는 %d부터 %d 사이의 숫자여야 합니다.");
 
     private final String message;
