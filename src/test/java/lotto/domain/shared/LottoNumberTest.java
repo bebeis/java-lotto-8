201400a6e0163be4aae78d5a64a630590ceaef11
Junseo Bae <ferrater1013@gmail.com>
2025-11-02 16:58:05 +0900

    test(LottoNumber): 로또 숫자 번호를 담당하는 LottoNumber 값 객체 구현
    - 최소, 최대 범위 내에 존재하는지 검증하는 테스트 추가
    - 최소, 최대 범위 밖에 존재하는 경우 예외를 검증하는 테스트 추가

---
+++ src/test/java/lotto/domain/shared/LottoNumberTest.java
@@ -0,0 +1,32 @@
+package lotto.domain.shared;
+
+import lotto.common.CommonErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LottoNumberTest {
+
+    @DisplayName("로또 번호는 1이상 45 이하의 값을 갖는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, 30, 20, 44, 45})
+    void lottoNumberMustBeBetween1And45(int value) {
+        // given && when
+        LottoNumber lottoNumber = new LottoNumber(value);
+
+        // then
+        assertThat(lottoNumber.number()).isEqualTo(value);
+    }
+
+    @DisplayName("로또 번호는 1미만 또는 45 초과의 값을 가질 수 없다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-10, 0, 46, 90})
+    void lottoNumberOutOfRangeThrowsException(int value) {
+        assertThatThrownBy(() -> new LottoNumber(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CommonErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE.message().formatted(1, 45));
+    }
+}
