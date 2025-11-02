9b4a7368cc2144e3f0e4e135fb6ebff39430b8c9
Junseo Bae <ferrater1013@gmail.com>
2025-11-02 16:21:42 +0900

    test(money): money VO 검증 테스트 추가
    - 음수 값을 검증하는 테스트 추가
    - 양수 값을 검증하는 테스트 추가

---
+++ src/test/java/lotto/domain/vo/MoneyTest.java
@@ -0,0 +1,31 @@
+package lotto.domain.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MoneyTest {
+
+    @DisplayName("돈은 음수 값을 가질 수 없다.")
+    @ParameterizedTest()
+    @ValueSource(ints = {-1, -300})
+    void moneyCannotHaveNegativeValue(int value) {
+        assertThatThrownBy(() -> new Money(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(CommonErrorMessage.MONEY_NEGATIVE_VALUE.message());
+    }
+
+    @DisplayName("돈은 0 이상의 값을 가질 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1000, 3000})
+    void moneyCanBeCreatedWithZeroOrPositiveValue(int value) {
+        // given && when
+        Money money = new Money(value);
+
+        // then
+        assertThat(money.value()).isEqualTo(value);
+    }
+
+}
