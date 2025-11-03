13edd03b61575039477b164cd689e2ddfe5f2d58
Junseo Bae <ferrater1013@gmail.com>
2025-11-03 14:07:17 +0900

    test: 보너스 번호 중복 검증 테스트 추가

--- src/test/java/lotto/domain/result/WinningLottoTest.java
+++ src/test/java/lotto/domain/result/WinningLottoTest.java
@@ -3,6 +3,7 @@ package lotto.domain.result;
 import lotto.domain.shared.Lotto;
 import lotto.domain.shared.LottoNumber;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 class WinningLottoTest {
@@ -137,5 +139,25 @@ class WinningLottoTest {
                         Rank.NONE
                 )
         );
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다")
+    void validateBonusNumberNotDuplicated() {
+        // given
+        Lotto winningNumbers = new Lotto(List.of(
+                new LottoNumber(1),
+                new LottoNumber(2),
+                new LottoNumber(3),
+                new LottoNumber(4),
+                new LottoNumber(5),
+                new LottoNumber(6)
+        ));
+        LottoNumber duplicatedBonusNumber = new LottoNumber(6);
+
+        // when & then
+        assertThatThrownBy(() -> new WinningLotto(winningNumbers, duplicatedBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ResultErrorMessage.BONUS_NUMBER_DUPLICATED_WITH_WINNING_NUMBERS.message());
     }
 }
