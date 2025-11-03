d1942b5c6a775ab8f7b970dfca452c73f6f2f5bf
Junseo Bae <ferrater1013@gmail.com>
2025-11-03 14:07:45 +0900

    feat: 보너스 번호 중복 예외 메시지 추가

--- src/main/java/lotto/domain/result/ResultErrorMessage.java
+++ src/main/java/lotto/domain/result/ResultErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.domain.result;
 import lotto.common.ErrorMessage;
 
 public enum ResultErrorMessage implements ErrorMessage {
-    INVALID_PURCHASE_AMOUNT_FOR_PROFIT("수익률을 계산하려면 구매 금액이 0원보다 커야 합니다");
+    INVALID_PURCHASE_AMOUNT_FOR_PROFIT("수익률을 계산하려면 구매 금액이 0원보다 커야 합니다"),
+    BONUS_NUMBER_DUPLICATED_WITH_WINNING_NUMBERS("보너스 번호는 당첨 번호와 중복될 수 없습니다");
 
     private final String message;
 
