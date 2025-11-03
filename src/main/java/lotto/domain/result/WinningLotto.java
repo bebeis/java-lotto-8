f725eedb37c48b0e02d54d74dcdb7ecb8c1e2606
Junseo Bae <ferrater1013@gmail.com>
2025-11-03 14:07:45 +0900

    feat: 보너스 번호 중복 검증 기능 추가

--- src/main/java/lotto/domain/result/WinningLotto.java
+++ src/main/java/lotto/domain/result/WinningLotto.java
@@ -8,8 +8,16 @@ public class WinningLotto {
     private final LottoNumber bonusNumber;
 
     public WinningLotto(final Lotto winningLotto, final LottoNumber bonusNumber) {
+        validateBonusNumberNotDuplicated(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumberNotDuplicated(final Lotto winningLotto, final LottoNumber bonusNumber) {
+        if (winningLotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException(ResultErrorMessage.BONUS_NUMBER_DUPLICATED_WITH_WINNING_NUMBERS
+                    .message());
+        }
     }
 
     public Rank findRankOf(final Lotto purchasedLotto) {
