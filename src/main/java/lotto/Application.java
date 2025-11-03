1d974354fb18d6641bcf807a5842f61aa7db01a8
Junseo Bae <ferrater1013@gmail.com>
2025-11-03 21:22:36 +0900

    refactor: try-finally 로 Console 자원 관리 개선

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.config.AppConfig;
 
 public class Application {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
-        
-        appConfig.purchaseLottosController().run();
-        appConfig.calculateWinningResultController().run();
-        appConfig.calculateProfitRateController().run();
+
+        try {
+            appConfig.purchaseLottosController().run();
+            appConfig.calculateWinningResultController().run();
+            appConfig.calculateProfitRateController().run();
+        } finally {
+            Console.close();
+        }
     }
 }
